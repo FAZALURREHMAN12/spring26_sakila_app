e409f9994b2866689330af5b70b99373b1c3f61b
Fazal <fazifazifazi12@gmail.com>
2026-04-21 19:07:46 +0500

    feat(config): add health check interval

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
+# Author: Taha Ameer, Date: 2026-04-21
 import os
 
 class Config:
-    MYSQL_HOST = os.environ.get('MYSQL_HOST', 'mysql-container')
+    MYSQL_HOST = os.environ.get('MYSQL_HOST', 'db-primary')
+    HEALTH_CHECK_INTERVAL = int(os.environ.get('HEALTH_CHECK_INTERVAL', '10'))
     MYSQL_USER = os.environ.get('MYSQL_USER', 'root')
     MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', 'admin')
     MYSQL_DB = os.environ.get('MYSQL_DB', 'sakila')
