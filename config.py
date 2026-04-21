8d044e8c5b6e2d84605ad50b89fdcf544089a6bd
Fazal <fazifazifazi12@gmail.com>
2026-04-21 19:02:09 +0500

    feat(config): update database host and add timeout setting

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 import os
 
 class Config:
-    MYSQL_HOST = os.environ.get('MYSQL_HOST', 'mysql-container')
+    MYSQL_HOST = os.environ.get('MYSQL_HOST', 'sakila-db-server')
+    CONNECTION_TIMEOUT = int(os.environ.get('CONNECTION_TIMEOUT', '30'))
     MYSQL_USER = os.environ.get('MYSQL_USER', 'root')
     MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', 'admin')
     MYSQL_DB = os.environ.get('MYSQL_DB', 'sakila')
